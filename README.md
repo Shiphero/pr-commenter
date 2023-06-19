08469e95bc1bfb0b241286d7e50b975a83784057
Martín Gaitán <gaitan@gmail.com>
2023-06-19 12:15:34 -0300

    refactor based on templates

--- README.md
+++ README.md
@@ -1,8 +1,108 @@
-# pr_commenter
+# pr-commenter
 
-Comment or update a comment in a given PR
+A CLI app to manage a comment a Github PR based on content from stdin, files or environment variables. 
 
-# Install
+You can create a new comment, delete previous one of the same type
+or update the "current" one. 
+
+An special comment with metadata is added to the comment so it could be 
+detected in a subsequent run and update or delete if needed. 
+
+## Basic usage
+
+Create a Github token with `repo` scope and export it as `PR_COMMENTER_GITHUB_TOKEN` environment variable
+
+```
+pr-commenter your/repo 12 comment.txt
+```
+
+Or from the standard input
+
+```
+cat comment.txt | pr-commenter your/repo 12 -
+```
+
+## Advanced example
+
+Suposse you have a template `failed_tests.j2` like this
+
+```jinja2
+{% if not is_append %}## Failed tests{% endif %}
+ 
+[suite {{ CI_SUITE_NAME }}]({{ CI_SUITE_URL }}):
+
+```bash
+{% for line in input_lines %}{{ line }}
+{% endfor %}
+```
+
+And your suite produce a file `failures.txt`  with the list 
+of failed tests like 
+
+```	
+tests/test_client.py::test_client_simple
+tests/test_client.py::test_client_complex
+```
+
+Suppose you want to add a single comment to the PR with the failed tests for 
+two different suites executions in the same "commit". 
+
+In the post-build phase, you can run
+
+```
+$ pr-commenter your/repo $PR failures.txt -t failed_tests.j2 --build=$COMMIT
+```
+
+The first suite that finish will post a like this
+
+
+````markdown
+<!-- pr_commenter: failed_tests.j2 abc123 -->
+## Failed tests
+ 
+[suite client](http://the-client-url):
+
+```bash
+tests/test_client.py::test_client_simple
+tests/test_client.py::test_client_complex
+```
+````
+
+Then the second suite will find the previous comment for this template 
+and commit (`"abc123"`, so the result will be an update like 
+
+````markdown
+<!-- pr_commenter: failed_tests.j2 abc123 -->
+## Failed tests
+ 
+[suite client](http://the-client-url):
+
+```bash
+tests/test_client.py::test_client_simple
+tests/test_client.py::test_client_complex
+```
+
+[suite server](http://the-server-url):
+
+```bash
+tests/test_server.py::test_server_1
+```
+
+````
+
+In a next commit, the first suite will post a new comment, but this time 
+the commit will be different, so a new comment will be created and the previous
+deleted. 
+
+## Templates
+
+It's use Jinja2 templates. The stdin/files is passed and a variable `input_lines`
+A `is_append` will be `True` in case the new comment will be appended to an existent
+one (so you can ommit some header). In addition, the complete `os.environ`
+dictionary is passed, so all the environment variables are available in the template. 
+
+
+## Install
 
 - Install the package
 
@@ -10,3 +110,8 @@ Comment or update a comment in a given PR
 pip install --user pipx
 pipx install  git+ssh://git@github.com/Shiphero/pr_commenter.git
 ```
+
+- Create a Github token with `repo` scope and export it as `PR_COMMENTER_GITHUB_TOKEN` environment variable
+
+
+
--- pr_commenter.py
+++ pr_commenter.py
@@ -1,68 +1,130 @@
 """Create or upgrade a comment in a given PR"""
 import os
-import sys
+import re
 from docopt import docopt
+from pathlib import Path
 import fileinput
 from github import Github, GithubException
+from os import environ
+from jinja2 import Environment
+import logging
+from rich.logging import RichHandler
+
 
 usage = """
 Usage:
   pr_commenter (-h | --help)
-  pr_commenter <repo> <pr> <file>... [--title=<title>] [--wrap=<language>] [--token=<token>] [--label=<label>...]
+  pr_commenter <repo> <pr> [<file>...] [--template=<template>] [--build=<build>] [--append] [--token=<token>] [--label=<label>...] [--debug]
 
 Options:
-  -h --help                         Show this screen.  
-  --title=<title>                   Title for the comment
-  --wrap=<language>                 Wrap the comment as code language
-  --token=<token>                   Github token. Default to the envvar PR_COMMENTER_GITHUB_TOKEN.
-  --label=<label>                   Add label/s if there were a comment
+  -h --help                     Show this screen.  
+  -t, --template=<template>     Use a given Jinja template.    
+  --token=<token>               Github token. Default to the envvar PR_COMMENTER_GITHUB_TOKEN.
+  --label=<label>               Add label/s if there were a comment
+  --build=<build>               Identifier. If a comment for the template and build exist the 
+                                new message will be appended.
+  --debug                       Show the final comment but don't post it to Github
 """
 
-__version__ = "0.1"
+__version__ = "0.2"
 
-def main(argv=None) -> None:
-    args = docopt(__doc__ + usage, argv, version=__version__)
+logger = logging.getLogger(__file__)
 
+
+def setup_logger(debug=False):
+    # setup logging
+    log_level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(
+        level=log_level, format="%(message)s", datefmt="->", handlers=[RichHandler()]
+    )
+    
+
+def get_pr_and_user(args):
     try:
         gh = Github(args["--token"] or os.environ["PR_COMMENTER_GITHUB_TOKEN"])
-    except KeyError:
-        raise SystemExit("Token not found. Pass --token or set envvar PR_COMMENTER_GITHUB_TOKEN")
+    except (KeyError, GithubException):
+        logger.fatal("Token not found or is invalid. Pass --token or set envvar PR_COMMENTER_GITHUB_TOKEN")
+        exit(1)
+    try:
+        user = gh.get_user().login
+        pr = gh.get_repo(args["<repo>"]).get_pull(int(args["<pr>"].strip("pr/")))
+        return pr, user
+    except (KeyError, GithubException):
+        logger.fatal("Repo or PR not found")
+        exit(1)
 
 
-    comment = ""
+def render(lines, template, build="", is_append=False):
+    """
+    Render the template passing environment variables and the lines as input_lines
+    """
+    env = Environment(autoescape=True)
+    t = env.from_string(Path(template).read_text())
+    comment = t.render({"input_lines": lines, "is_append": is_append, **environ})
+    logger.debug("Comment:\n%s", comment)
+    if is_append:
+        return comment
+    return f"<!-- pr_commenter: {template} {build or ''} -->\n{comment}"
+    
+    
+def main(argv=None) -> None:
+    args = docopt(__doc__ + usage, argv, version=__version__)
+    debug = args["--debug"]
+
+    setup_logger()
+
+    pr, user = get_pr_and_user(args)
+
+    lines = []
     with fileinput.input(files=args["<file>"]) as f:
         for line in f:
-            comment += line
+            lines.append(line.strip())
     
-    comment = comment.strip()
-    if not comment:
-        print("No comment found. Exiting.")
-        return
-
-    wrap_start = f"```{args['--wrap']}\n" if args['--wrap'] else ""
-    wrap_end = f"\n```" if args['--wrap'] else ""
-    title = f"{args['--title']}" if args["--title"] else "[AUTOMATIC COMMENT]"
-    
-    comment = f"{title}\n\n{wrap_start}{comment}{wrap_end}"
-
-
-    pr = gh.get_repo(args["<repo>"]).get_pull(int(args["<pr>"].strip("pr/")))
-
     # update or create a comment
-    existent_comment = None    
-    for c_issue in pr.get_issue_comments():
-        if c_issue.body.startswith(title):
-            existent_comment = c_issue
-            break
-    if existent_comment:
-        existent_comment.edit(comment)
-        print(f"Comment updated: {existent_comment.html_url}")
-    else:
-        issue_comment = pr.create_issue_comment(comment)
-        print(f"Comment created: {issue_comment.html_url}")
+    comment = None
+    for previous_comment in pr.get_issue_comments():
+        if previous_comment.user.login != user:
+            # only consider comments from the same user
+            continue
 
-    if args["--label"]:        
-        print(f"Labels added: {', '.join(args['--label'])}")
+        first_line = previous_comment.body.split("\n")[0]
+
+        match = re.search(r"<!-- pr_commenter: (\S+) (\S+)?\s*-->", first_line)
+        if match:
+            prev_template, previous_build = match.groups()
+            if prev_template == args["--template"] and previous_build != args["--build"]:
+                logger.info("Found a previous comment for a different build. Deleting...")
+                previous_comment.delete()
+                break
+            elif prev_template == args["--template"] and previous_build == args["--build"]:
+                logger.info("Found a previous comment for the same build. Appending...")
+                new_comment = render(lines, args["--template"], args["--build"], is_append=True)
+                comment = f"{previous_comment.body}\n{new_comment}"
+                
+                logger.debug(f"Updated comment: {comment}")
+                if not debug:
+                    previous_comment.edit(comment)
+                    logger.info(f"Comment updated: {previous_comment.html_url}")
+                break
+
+    is_empty = False
+    if not comment:
+        comment = render(lines, args["--template"], args["--build"])
+
+        is_empty = re.sub(r"<!-- pr_commenter[^>]*-->", "", comment).strip()
+        if is_empty:
+            logger.info("New comment is empty. Skipping...")
+            # remove labels 
+            for label in args["--label"]:
+                logger.debug(f"Removing label: {label}")
+                pr.remove_from_labels(label)
+        else: 
+            issue_comment = pr.create_issue_comment(comment)
+            logger.debug(f"New comment: {comment}")
+            logger.info(f"Comment created: {issue_comment.html_url}")
+
+    if not is_empty and args["--label"]:        
+        logger.info(f"Labels added: {', '.join(args['--label'])}")
         pr.add_to_labels(*args["--label"])
 
 
